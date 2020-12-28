ce468ab629acf3cab7901d097f2da04501f62f9f
Tawhid <gem@tawhid.fios-router.home>
2020-12-28 11:31:14 -0500

    added google font

---
+++ src/AppBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function AppBar(props) {
+    return (
+        <div>
+            <div>Crypto Dash</div>
+            <div />
+            <div>Settings</div>
+            <div>Dashboard</div>
+        </div>
+    )
+}
