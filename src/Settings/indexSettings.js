5da67f9cfa6f89a2feb55ed73e623a95f1e8980e
Tawhid <gem@tawhid.fios-router.home>
2021-01-05 17:57:39 -0500

    added toggle for dashboard

--- src/Settings/indexSettings.js
+++ src/Settings/indexSettings.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ConfirmButton from './ConfirmButton'
 import WelcomeMessage from './WelcomeMessage'
+import Page from '../Shared/Page'
 
 export default function indexSettings() {
     return (
-        <div>
+        <Page name='settings'>
             <WelcomeMessage />
             <ConfirmButton />
-        </div>
+        </Page>
     )
 }
