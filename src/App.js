24ab36575933d5ddcf00e32b96858537569b2447
Tawhid <gem@tawhid.fios-router.home>
2021-01-09 16:03:27 -0500

    added loading feature

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import AppLayout from './AppLayout';
 import IndexSettings from './Settings/indexSettings';
 import AppBar from './AppBar'
 import {AppProvider} from './AppProvider'
+import Content from './Shared/Content'
 
 function App() {
   return (
     <AppLayout>
       <AppProvider>
         <AppBar />
-        <IndexSettings />
+
+        <Content>
+          <IndexSettings />
+        </Content>
       </AppProvider>
     </AppLayout>
   );
---
+++ src/Shared/Content.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {AppContext} from '../AppProvider'
+
+export default function Content(props) {
+    return (
+        <AppContext.Consumer>
+            {
+                ({coinList}) => {
+                    if(!coinList) {
+                        return <div>Loading Coins</div>
+                    }
+
+                    return <div> {props.children} </div>
+                }
+            }
+        </AppContext.Consumer>
+    )
+}
