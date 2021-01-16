948893f22e9cd900b1ef88eb631435d178a25611
Tawhid <gem@tawhid.fios-router.home>
2021-01-15 21:45:18 -0500

    responsive grid

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -5,7 +5,7 @@ import CoinTile from './CoinTile'
 
 const CoinGridStyled = styled.div`
     display: grid;
-    grid-template-columns: repeat(5, 1fr);
+    grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
     grid-gap: 15px;
     margin-top: 43.5px;
 `
