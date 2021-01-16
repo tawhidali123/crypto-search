c38cdc1eceaa0f5f86c001a7f01d5d3e18bd00fd
Tawhid <gem@tawhid.fios-router.home>
2021-01-16 15:09:48 -0500

    settings done

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -5,23 +5,27 @@ import CoinTile from './CoinTile'
 
 const CoinGridStyled = styled.div`
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
     grid-gap: 15px;
     margin-top: 43.5px;
 `
 
-function getCoinsToDisplay(coinList, topSection, favorites) {
-    return topSection ? favorites : Object.keys(coinList).slice(0, 100);
+function getLowerSectionCoins(coinList, filteredCoins) {
+    return( filteredCoins && Object.keys(filteredCoins) || Object.keys(coinList).slice(0,100) )
+}
+
+function getCoinsToDisplay(coinList, topSection, favorites, filteredCoins) {
+    return topSection ? favorites : getLowerSectionCoins(coinList, filteredCoins);
 }
 
 export default function CoinGrid({topSection}) {
     return (
         <AppContext.Consumer>
             {
-                ({coinList, favorites}) => (
+                ({coinList, favorites, filteredCoins}) => (
                     <CoinGridStyled>
                         {
-                            getCoinsToDisplay(coinList, topSection, favorites).map(coinKey => {
+                            getCoinsToDisplay(coinList, topSection, favorites, filteredCoins).map(coinKey => {
                                 return <CoinTile coinKey={coinKey} topSection={topSection} />
                             })
                         }
